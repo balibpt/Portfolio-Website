fd7fd8a13dfaa9dcc312f751d3bc5200b4cdcfc3
Bali Tok <bali.tok@hotmail.com>
2023-04-19 19:40:58 +0800

    added skills component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
       <Banner />
+      <Skills />
     </React.Fragment>
   );
 }
