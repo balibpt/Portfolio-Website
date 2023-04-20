8a52c42bcb2d4de3321290ba3662ee689dc9c344
Bali Tok <bali.tok@hotmail.com>
2023-04-20 17:26:14 +0800

    installed fontawesome

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,13 @@
 import React from "react";
+import footerBg from "../assets/footer-bg.png";
 
 export default function Footer() {
   return (
     <React.Fragment>
-      <div></div>
+      <div
+        className="py-20"
+        style={{ backgroundImage: `url(${footerBg})` }}
+      ></div>
     </React.Fragment>
   );
 }
