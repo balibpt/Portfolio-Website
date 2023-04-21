8eaa3e8b2b6a0904bdf975562fee1476fada1881
Bali Tok <bali.tok@hotmail.com>
2023-04-21 14:38:35 +0800

    Added projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,21 @@
-import React from "react";
-import { Carousel } from "flowbite-react";
-import porjectImg1 from "../assets/project-img1.png";
+import React, { useState, useEffect } from "react";
+import { Carousel, Modal } from "flowbite-react";
+import porjectImg3 from "../assets/projectImg3.png";
+import airbnb from "../assets/airbnb.png";
+import airbnbGif from "../assets/airbnb.gif";
+import catchGif from "../assets/catch.gif";
 
 export default function Projects() {
+  const [showCatchModal, setShowCatchModal] = useState(false);
+  const [showAirbnbModal, setShowAirbnbModal] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("click", () => {
+      setShowCatchModal(false);
+      setShowAirbnbModal(false);
+    });
+  }, []);
+
   return (
     <React.Fragment>
       <div id="projects" className="py-4 px-6 mb-10">
@@ -16,26 +29,72 @@ export default function Projects() {
         </p>
         <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 px-16">
           <Carousel>
-            <img
-              src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
-              alt="..."
-            />
-            <img
-              src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
-              alt="..."
-            />
-            <img
-              src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-              alt="..."
-            />
-            <img
-              src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-              alt="..."
-            />
-            <img
-              src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-              alt="..."
-            />
+            <div
+              onMouseEnter={() => {
+                setShowCatchModal(true);
+              }}
+            >
+              <a
+                href="https://catch-nft.herokuapp.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src={porjectImg3}
+                  alt="..."
+                  className="object-fill opacity-80"
+                />
+                <Modal show={showCatchModal}>
+                  <Modal.Body
+                    onMouseLeave={() => {
+                      setShowCatchModal(false);
+                    }}
+                  >
+                    <a
+                      href="https://catch-nft.herokuapp.com/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="block h-full"
+                    >
+                      <img src={catchGif} alt="" className="" />
+                    </a>
+                  </Modal.Body>
+                </Modal>
+              </a>
+            </div>
+            <div
+              onMouseEnter={() => {
+                setShowAirbnbModal(true);
+              }}
+            >
+              <a
+                href="https://github.com/wenghaishi/1160-airbnb-clone"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src={airbnb}
+                  alt="..."
+                  className="object-fill opacity-80"
+                />
+                <Modal show={showAirbnbModal}>
+                  <Modal.Body
+                    onMouseLeave={() => {
+                      setShowAirbnbModal(false);
+                    }}
+                  >
+                    <a
+                      href="https://github.com/wenghaishi/1160-airbnb-clone"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="block h-full"
+                    >
+                      <img src={airbnbGif} alt="" className="" />
+                    </a>
+                  </Modal.Body>
+                </Modal>
+              </a>
+            </div>
           </Carousel>
         </div>
       </div>
